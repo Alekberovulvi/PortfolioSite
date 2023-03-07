d18e5a88cb7326c85c0277e0210e70095835ee22
Alekberovulvi <ulvina@code.edu.az>
2023-03-07 23:21:29 +0400

    PortfolioWebSite

--- PortfolioWebSite/PortfolioWebSite/Areas/Manage/Controllers/CvController.cs
+++ PortfolioWebSite/PortfolioWebSite/Areas/Manage/Controllers/CvController.cs
@@ -4,6 +4,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using MimeKit.Cryptography;
 using PortfolioWebSite.DAL;
+using PortfolioWebSite.Models;
 using PortfolioWebSite.ViewsModel;
 
 namespace PortfolioWebSite.Areas.Manage.Controllers
@@ -45,85 +46,51 @@ namespace PortfolioWebSite.Areas.Manage.Controllers
                 return View();
             }
 
-           
+            var allowedExtensions = new[] { ".pdf", ".doc", ".docx" };
+            var fileExtension = Path.GetExtension(file.FileName);
+            if (!allowedExtensions.Contains(fileExtension))
+            {
+                ModelState.AddModelError("file", "Invalid file format. Only PDF, DOC, and DOCX files are allowed.");
+                return View();
+            }
 
-            var filename = Path.GetFileNameWithoutExtension(file.FileName) + "_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + Path.GetExtension(file.FileName);
+
+            var filename = Path.GetFileNameWithoutExtension(file.FileName) + Path.GetExtension(file.FileName);
             var filePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/assets/cv",filename);
 
             using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
+
+            var cv = new Cv
+            {
+                FileName = filename,
+                Name = file.FileName
+            };
+
+            _context.Cvs.Add(cv);
+            _context.SaveChanges();
+
             return RedirectToAction("Index");
         }
+        
 
         [HttpGet]
-        public async Task<IActionResult> Update()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Update(int id , IFormFile file)
+        public async Task<IActionResult> Delete(int id)
         {
             var cv = await _context.Cvs.FindAsync(id);
-
             if (cv == null)
-            {
                 return NotFound();
-            }
 
-            if (file == null || file.Length == 0) 
-            {
-                ModelState.AddModelError("file", "Please select a file.");
-                return View(cv);
-            }
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "assets", "cv", cv.FileName);
 
-            var allowedExtensions = new[] { ".pdf", ".doc", ".docx" };
-            var fileExtension = Path.GetExtension(file.FileName);
-            if (allowedExtensions.Contains(fileExtension))
-            {
-                ModelState.AddModelError("file", "Invalid file format. Only PDF, DOC, and DOCX files are allowed.");
-                return View(cv);
-            }
+            if (System.IO.File.Exists(path))
+                System.IO.File.Delete(path);
 
-            var filename = Path.GetFileNameWithoutExtension(file.FileName) + "_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + fileExtension;
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/assets/cv", filename);
-
-            using (var stream =new FileStream(filePath, FileMode.Create))
-            {
-                await file.CopyToAsync(stream);
-            }
-
-            var oldfilePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/assets/cv",cv.FileName);
-            if (System.IO.File.Exists(oldfilePath))
-            {
-                System.IO.File.Delete(oldfilePath);
-            }
-
-            cv.FileName= filename;
-            _context.SaveChanges();
+            _context.Cvs.Remove(cv);
+            await _context.SaveChangesAsync();
             return RedirectToAction("Index");
-
         }
-
-
-        //[HttpGet]
-        //public async Task<IActionResult> Delete(int id)
-        //{
-        //    var cv = await _context.Cvs.FindAsync(id);
-        //    if (cv == null)
-        //        return NotFound();
-
-        //    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "assets", "cv", cv.FileName);
-
-        //    if (System.IO.File.Exists(path))
-        //        System.IO.File.Delete(path);
-
-        //    _context.Cvs.Remove(cv);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
     }
 }
